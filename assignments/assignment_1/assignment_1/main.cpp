3d26c6a260547da66bcc01219e3626151bbe4dca
xyjiang970 <jason@macbookpro.mynetworksettings.com>
2025-09-14 13:26:41 -0400

    .

---
+++ assignments/assignment_1/assignment_1/main.cpp
@@ -0,0 +1,25 @@
+//  csc331-section_501L prog1 xin yu (jason) jiang 
+//  assignment_1
+//  due date sep 19, 2025
+
+#include <iostream>
+#include "./include/personType.h"
+
+int main() {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+
+        
+    /* 
+    Program will wait for the user to press a key before continuing, 
+    which will give you time to examine your program’s output before your 
+    operating system closes the console window.
+    */
+    std::cin.clear(); // reset any error flags
+     // ignore any characters in the input buffer until we find an enter character
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    std::cout << "Press any key to exit..." << std::endl;
+    std::cin.get(); // get one more char from the user
+    
+    return 0;
+}
